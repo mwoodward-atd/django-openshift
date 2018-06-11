26d50debcf59af03b0381288e6c15b53998c360d
Matt Woodward <matthew_woodward@saa.senate.gov>
2018-06-11 13:48:47 -0700

    messing with postgres settings

--- djangotest2/settings.py
+++ djangotest2/settings.py
@@ -74,13 +74,24 @@ WSGI_APPLICATION = 'djangotest2.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('database_name'),
+        'USER': os.getenv('username'),
+        'PASSWORD': os.getenv('password'),
+        'HOST': 'postgresql',
+        'PORT': 5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
